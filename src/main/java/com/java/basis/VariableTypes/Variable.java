faf21033b0e0de8561fad586b323986f6d0ac1f3
lizhen <595265454@qq.com>
2018-10-01 23:19:55 +0800

    java变量

---
+++ src/main/java/com/java/basis/VariableTypes/Variable.java
@@ -0,0 +1,24 @@
+package com.java.basis.VariableTypes;
+
+/**
+ * @Author: Lambert
+ * @Date: 2018/10/1 下午10:51
+ * @Description: java 变量
+ */
+public class Variable {
+    /**
+     * 类变量
+     */
+    static int allClicks = 0;
+    /**
+     * 实例变量
+     */
+    String str = "hello world";
+
+    public void method() {
+        /**
+         * 局部变量
+         */
+        int i = 0;
+    }
+}
