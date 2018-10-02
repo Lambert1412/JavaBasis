deb60b05c9209dd6684f1f575d67da76c516821a
lizhen <595265454@qq.com>
2018-10-02 14:32:39 +0800

    关系运算符

---
+++ src/main/java/com/java/basis/operator/RelationshipBetweenTest.java
@@ -0,0 +1,21 @@
+package com.java.basis.operator;
+
+/**
+ * @Author: Lambert
+ * @Date: 2018/10/2 下午2:30
+ * @Description:
+ * 关系运算符：
+ *
+ */
+public class RelationshipBetweenTest {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println("a == b = " + (a == b) );
+        System.out.println("a != b = " + (a != b) );
+        System.out.println("a > b = " + (a > b) );
+        System.out.println("a < b = " + (a < b) );
+        System.out.println("b >= a = " + (b >= a) );
+        System.out.println("b <= a = " + (b <= a) );
+    }
+}
