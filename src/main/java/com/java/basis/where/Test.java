8fa4cf748a13d5d9ed6f367389079424d043bbf1
lizhen <595265454@qq.com>
2018-10-03 15:47:49 +0800

    where 循环

---
+++ src/main/java/com/java/basis/where/Test.java
@@ -0,0 +1,18 @@
+package com.java.basis.where;
+
+/**
+ * @Author: Lambert
+ * @Date: 2018/10/2 下午4:32
+ * @Description:
+ * where 循环
+ */
+public class Test {
+    public static void main(String args[]) {
+        int x = 10;
+        while (x < 20) {
+            System.out.print("value of x : " + x);
+            x++;
+            System.out.print("\n");
+        }
+    }
+}
