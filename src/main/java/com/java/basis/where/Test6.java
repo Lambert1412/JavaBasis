bea3e9bb7e14fa55d0d350276bd7d545f950726d
lizhen <595265454@qq.com>
2018-10-03 15:54:36 +0800

    if语句 用法

---
+++ src/main/java/com/java/basis/where/Test6.java
@@ -0,0 +1,17 @@
+package com.java.basis.where;
+
+/**
+ * @Author: Lambert
+ * @Date: 2018/10/2 下午9:05
+ * @Description:
+ * if语句
+ */
+public class Test6 {
+    public static void main(String args[]){
+        int x = 10;
+
+        if( x < 20 ){
+            System.out.print("这是 if 语句");
+        }
+    }
+}
