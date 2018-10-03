45dc8b4e4447308c919b85a94ff991bd82af7fb7
lizhen <595265454@qq.com>
2018-10-03 19:51:32 +0800

    对象比较大小

---
+++ src/main/java/com/java/basis/method/TestMax.java
@@ -0,0 +1,29 @@
+package com.java.basis.method;
+
+import static java.lang.Math.max;
+
+/**
+ * @Author: Lambert
+ * @Date: 2018/10/3 下午7:46
+ * @Description:
+ * 对象比较大小
+ */
+public class TestMax {
+    public static void main(String[] args) {
+        int i=5;
+        int j=2;
+        int k=max(i,j);
+        System.out.println(i+"和"+j+"比较，最大值是："+k);
+    }
+    /** 返回两个整数变量较大的值 */
+    /** 返回两个整数变量较大的值 */
+    public static int max(int num1, int num2) {
+        int result;
+        if (num1 > num2)
+            result = num1;
+        else
+            result = num2;
+
+        return result;
+    }
+}
