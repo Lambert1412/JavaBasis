a51db2b1e8a8eaffa805a590998e2abec7dbbe29
lizhen <595265454@qq.com>
2018-10-03 17:28:01 +0800

    处理数组

---
+++ src/main/java/com/java/basis/Number/TestArray1.java
@@ -0,0 +1,30 @@
+package com.java.basis.Number;
+
+/**
+ * @Author: Lambert
+ * @Date: 2018/10/3 下午5:22
+ * @Description:
+ * 处理数组
+ * 数组的元素类型和数组的大小都是确定的，所以当处理数组元素时候，我们通常使用基本循环或者 foreach 循环。
+ */
+public class TestArray1 {
+    public static void main(String[] args) {
+        double[] myList={1.9, 2.9, 3.4, 3.5};
+        // 打印所有数组元素
+        for (int i = 0; i < myList.length; i++) {
+            System.out.println(myList[i] + " ");
+        }
+        // 计算所有元素的总和
+        double total = 0;
+        for (int i = 0; i < myList.length; i++) {
+            total += myList[i];
+        }
+        System.out.println("Total is " + total);
+        // 查找最大元素
+        double max = myList[0];
+        for (int i = 1; i < myList.length; i++) {
+            if (myList[i] > max) max = myList[i];
+        }
+        System.out.println("Max is " + max);
+    }
+    }
