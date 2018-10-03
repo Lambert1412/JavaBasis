6881f7e06f1c460e3cc748a3def3f97efd8e0445
lizhen <595265454@qq.com>
2018-10-03 17:03:01 +0800

    字符串长度

---
+++ src/main/java/com/java/basis/Number/StringDemo1.java
@@ -0,0 +1,18 @@
+package com.java.basis.Number;
+
+/**
+ * @Author: Lambert
+ * @Date: 2018/10/3 下午4:59
+ * @Description:
+ *
+ * 字符串长度
+ * 用于获取有关对象的信息的方法称为访问器方法。
+ * String 类的一个访问器方法是 length() 方法，它返回字符串对象包含的字符数。
+ */
+public class StringDemo1 {
+    public static void main(String[] args) {
+        String site="HelloWord";
+        int len =site.length();
+        System.out.println("上面字符串长度是"+len);
+    }
+}
