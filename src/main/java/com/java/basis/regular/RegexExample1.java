51684847577217df4bf754556530e27abd28fc2b
lizhen <595265454@qq.com>
2018-10-03 19:07:03 +0800

    Pattern 正则使用

--- src/main/java/com/java/basis/regular/RegexExample1.java
+++ src/main/java/com/java/basis/regular/RegexExample1.java
@@ -8,7 +8,9 @@ import java.util.regex.Pattern;
  * @Description:
  * Pattern 类：
  * pattern 对象是一个正则表达式的编译表示。
- * Pattern 类没有公共构造方法。要创建一个 Pattern 对象，你必须首先调用其公共静态编译方法，它返回一个 Pattern 对象。该方法接受一个正则表达式作为它的第一个参数。
+ * Pattern 类没有公共构造方法。
+ * 要创建一个 Pattern 对象，你必须首先调用其公共静态编译方法，它返回一个 Pattern 对象。
+ * 该方法接受一个正则表达式作为它的第一个参数。
  */
 public class RegexExample1 {
     public static void main(String[] args) {
