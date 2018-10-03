9f2e5fdc2efe01d1a7d7f1fe5c4bac30ac01960c
lizhen <595265454@qq.com>
2018-10-03 16:19:13 +0800

    case 不同用法

---
+++ src/main/java/com/java/basis/where/Test12.java
@@ -0,0 +1,28 @@
+package com.java.basis.where;
+
+/**
+ * @Author: Lambert
+ * @Date: 2018/10/3 下午4:15
+ * @Description:
+ *
+ * 如果当前匹配成功的 case 语句块没有 break 语句，则从当前 case 开始，
+ * 后续所有 case 的值都会输出，如果后续的 case 语句块有 break 语句则会跳出判断。
+ */
+
+public class Test12 {
+    public static void main(String[] args) {
+        int i=1;
+        switch (i){
+            case 0:
+                System.out.println("0");
+            case 1:
+                System.out.println("1");
+            case 2:
+                System.out.println("2");
+            case 3:
+                System.out.println("3");break;
+                default:
+                    System.out.println("default");
+        }
+    }
+}
