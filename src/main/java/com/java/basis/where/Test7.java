3034dcdf070aa6af9396500a48d0bdeb4eeb257e
lizhen <595265454@qq.com>
2018-10-03 15:54:54 +0800

    if…else 的用法如下

---
+++ src/main/java/com/java/basis/where/Test7.java
@@ -0,0 +1,19 @@
+package com.java.basis.where;
+
+/**
+ * @Author: Lambert
+ * @Date: 2018/10/2 下午9:06
+ * @Description:
+ * if…else 的用法如下
+ */
+public class Test7 {
+    public static void main(String args[]){
+        int x = 30;
+
+        if( x < 20 ){
+            System.out.print("这是 if 语句");
+        }else{
+            System.out.print("这是 else 语句");
+        }
+    }
+}
