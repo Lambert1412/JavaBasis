c3896dfd7e7c3dccfa6c248281564c5dc2b31e1b
lizhen <595265454@qq.com>
2018-10-03 16:58:30 +0800

    Spring 遍历循环

---
+++ src/main/java/com/java/basis/Number/StringDemo.java
@@ -0,0 +1,15 @@
+package com.java.basis.Number;
+
+/**
+ * @Author: Lambert
+ * @Date: 2018/10/3 下午4:53
+ * @Description:
+ * Spring 遍历循环
+ */
+public class StringDemo {
+    public static void main(String[] args) {
+        char [] helloArray = {'h','e','l','l','o','!'};
+        String helloString = new String(helloArray);
+        System.out.println(helloString);
+    }
+}
