817a10bbe582bab3bc0c621e3a5bf271cd9006ca
lizhen <595265454@qq.com>
2018-10-04 14:58:12 +0800

    写入控制台

---
+++ src/main/java/com/java/basis/File/WriteDemo.java
@@ -0,0 +1,20 @@
+package com.java.basis.File;
+
+/**
+ * @Author: Lambert
+ * @Date: 2018/10/4 下午2:54
+ * @Description:
+ * 写入控制台
+ *
+ */
+
+//演示 System.out.write().
+
+public class WriteDemo {
+    public static void main(String[] args) {
+        int b;
+        b='A';
+        System.out.write(b);
+        System.out.write('\n');
+    }
+}
