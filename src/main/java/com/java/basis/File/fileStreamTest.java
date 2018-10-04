94249e8e37dee4529d33c14954f0142700640908
lizhen <595265454@qq.com>
2018-10-04 15:15:22 +0800

    ？？？？？？

---
+++ src/main/java/com/java/basis/File/fileStreamTest.java
@@ -0,0 +1,32 @@
+package com.java.basis.File;
+import java.io.*;
+
+/**
+ * @Author: Lambert
+ * @Date: 2018/10/4 下午3:01
+ * @Description:
+ * ？？？？？？
+ */
+public class fileStreamTest {
+    public static void main(String args[]) {
+        try {
+            byte bWrite[] = { 11, 21, 3, 40, 5 };
+            OutputStream os = new FileOutputStream("test.txt");
+            for (int x = 0; x < bWrite.length; x++) {
+                // writes the bytes
+                os.write(bWrite[x]);
+            }
+            os.close();
+
+            InputStream is = new FileInputStream("test.txt");
+            int size = is.available();
+
+            for (int i = 0; i < size; i++) {
+                System.out.print((char) is.read() + "  ");
+            }
+            is.close();
+        } catch (IOException e) {
+            System.out.print("Exception");
+        }
+    }
+}
