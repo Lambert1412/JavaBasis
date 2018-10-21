6306c2f86dcf2681349046337f38adb3cae393b0
lizhen <595265454@qq.com>
2018-10-21 16:42:51 +0800

    优化最小值

--- src/main/java/com/java/basis/array/Test5.java
+++ src/main/java/com/java/basis/array/Test5.java
@@ -16,14 +16,17 @@ public class Test5 {
         /**
          * 定义数组
          */
-        int[] array = {2, 4,234, 6, 2, 4, 91};
+        int[] array = {2, 4, 234, 6, 2, 4, 91};
         int maxVal = maxValue(array);
+        int minVal = mimValue(array);
         System.out.println("最大值是：" + maxVal);
+        System.out.println("最小值是：" + minVal);
 
     }
 
     /**
      * 定义选举对象
+     *
      * @param array
      * @return
      */
@@ -39,6 +42,25 @@ public class Test5 {
             if (result < array[i]) {
                 /**
                  * 把比result大的数赋给result即可
+                 */
+                result = array[i];
+            }
+        }
+        return result;
+    }
+
+    public static int mimValue(int[] array) {
+        /**
+         * 定义结果变量
+         */
+        int result = array[0];
+        for (int i = 1; i < array.length; i++) {
+            /**
+             * 如果result 大于所比较的数的时候
+             */
+            if (result > array[i]) {
+                /**
+                 * 把比result小的数赋给result即可
                  */
                 result = array[i];
             }
