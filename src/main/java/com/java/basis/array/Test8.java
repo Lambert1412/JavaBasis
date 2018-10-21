726b762238352b5ae357cfa70daa1867ad95c817
lizhen <595265454@qq.com>
2018-10-21 23:06:41 +0800

    添加注释

--- src/main/java/com/java/basis/array/Test8.java
+++ src/main/java/com/java/basis/array/Test8.java
@@ -8,22 +8,40 @@ package com.java.basis.array;
  */
 public class Test8 {
     public static void main(String[] args) {
+        /**
+         * 定义数组
+         */
         int[] array = {23, 34, 87, 12, 7, 83, 57};
-        int index = getValueIndex(array, 58);
+        int index = getValueIndex(array, 57);
         System.out.println(index + 1);
     }
 
     public static int getValueIndex(int[] array, int value) {
-        int index = -1;
+        /**
+         * 定义结果索引
+         */
+        int index = 1;
+        /**
+         * 遍历数组
+         */
         for (int i = 0; i < array.length; i++) {
+            /**
+             * 如果找到了目标值
+             */
             if (array[i] == value) {
+                /**
+                 * 把索引赋给结果值
+                 */
                 index = i;
-                break;
-            } else {
-                System.out.println("查询不到");
+                /**
+                 * 跳出循环
+                 */
                 break;
             }
         }
+        /**
+         * 返回结果
+         */
         return index;
     }
 }
