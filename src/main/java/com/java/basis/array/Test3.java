635db99be12ede8ba90d5040aed5ee79dea78cb4
lizhen <595265454@qq.com>
2018-10-21 16:07:20 +0800

    数组第三种定义方式

---
+++ src/main/java/com/java/basis/array/Test3.java
@@ -0,0 +1,16 @@
+package com.java.basis.array;
+
+/**
+ * @Author: Lambert
+ * @Date: 2018/10/21 下午3:43
+ * @Description: java 数组
+ * 第三种定义方式
+ */
+public class Test3 {
+    public static void main(String[] args) {
+    char [] arr=new char[]{'a','b','c'};
+        for (int i = 0; i <arr.length ; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
