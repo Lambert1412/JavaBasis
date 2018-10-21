bb130dd547ea03ee1ded3151a6e8c8d52c17cc7d
lizhen <595265454@qq.com>
2018-10-21 15:41:08 +0800

    格式调整

--- src/main/java/com/java/basis/Override/Test3.java
+++ src/main/java/com/java/basis/Override/Test3.java
@@ -9,23 +9,25 @@ package com.java.basis.Override;
  */
 public class Test3 {
     public static void main(String[] args) {
-        int result = add(3,9);
-        double result1=add(4.5,8);
-        System.out.printf(result+"\n");
-        System.out.printf(result1+"\n");
+        int result = add(3, 9);
+        double result1 = add(4.5, 8);
+        System.out.printf(result + "\n");
+        System.out.printf(result1 + "\n");
     }
 
     /**
      * 比较两个数值的大小
+     *
      * @param a
      * @param b
      * @return
      */
-    public static int add(int a,int b){
-        return a+b;
+    public static int add(int a, int b) {
+        return a + b;
     }
-   public static double add(double a, int b){
-        return a+b;
-   }
+
+    public static double add(double a, int b) {
+        return a + b;
+    }
 
 }
