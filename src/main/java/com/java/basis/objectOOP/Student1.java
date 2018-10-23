59fd515ded43b6bc6d33d4b6e65c29e3ae20fef9
lizhen <595265454@qq.com>
2018-10-23 09:08:57 +0800

    修改封装

--- src/main/java/com/java/basis/objectOOP/Student1.java
+++ src/main/java/com/java/basis/objectOOP/Student1.java
@@ -2,11 +2,10 @@ package com.java.basis.objectOOP;
 
 /**
  * @Author: Lambert
- * @Date: 2018/10/23 上午8:44
- * @Description: private 封装
- * 使用private关键字完成属性私有化，这种属性只有本类能访问。
+ * @Date: 2018/10/23 上午8:59
+ * @Description:
  */
-public class Student {
+public class Student1 {
     /**
      * 姓名
      */
@@ -20,6 +19,10 @@ public class Student {
      */
     int age;
 
+    public void setName(String a) {
+        name = a;
+    }
+
 
     public void study() {
         System.out.println(name + "在学习");
@@ -30,17 +33,14 @@ public class Student {
     }
 }
 
-class TestStu {
+class TestStu1 {
     /**
      * public公共的所有类都能访问
      *
      * @param args
      */
     public static void main(String[] args) {
-        Student student = new Student();
-/**
- * 这就是封装
- */
-//        student.name = "我叫张三";
+        Student1 student1 = new Student1();
+        student1.setName("张三");
     }
 }
