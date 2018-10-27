93f272b0cfc7aa0813b1eb5c56de76e730ed6140
李真 <lizhen04842@hellobike.com>
2018-10-27 22:14:39 +0800

    构造器使用方法

--- src/main/java/com/java/basis/objectOOP/Person.java
+++ src/main/java/com/java/basis/objectOOP/Person.java
@@ -19,10 +19,16 @@ public class Person {
      * 1、方法名和类名一致；
      * 2、方法没有返回值没有void；
      * 3、参数可有可无；
+     * 4、如果类中没有参数的构造器，可有使用隐藏的默认构造器来创造对象
      *
      * @param name
      */
 
+    /**
+     * 有参构造器
+     *
+     * @param name
+     */
     public Person(String name) {
         System.out.println("一个人被创建");
         this.name = name;
@@ -30,12 +36,28 @@ public class Person {
 
     /**
      * 两个方法一样说明方法重载
+     *
      * @param name
      * @param age
      */
     public Person(String name, int age) {
-        this.name = name;
+//        this.name = name;
+        this(name);
         this.age = age;
+    }
+
+    public Person(String name, int age, char gender) {
+//        this.name = name;
+//        this.age = age;
+        //调用两个参数的构造器
+        this(name, age);
+        this.gender = gender;
+    }
+
+    /**
+     * 无参构造器
+     */
+    public Person() {
     }
 
     public void introuce() {
