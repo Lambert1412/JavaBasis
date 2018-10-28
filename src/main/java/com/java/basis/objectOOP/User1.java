8488caa70275e2473e7b1b1f6970478962de2f4b
李真 <lizhen04842@hellobike.com>
2018-10-28 15:48:43 +0800

    final 修饰符修饰在方法上

---
+++ src/main/java/com/java/basis/objectOOP/User1.java
@@ -0,0 +1,43 @@
+package com.java.basis.objectOOP;
+
+
+/**
+ * @program: basis
+ * @description:
+ * @author: Mr.LI
+ * @create: 2018-10-28 15:34
+ * final 使用
+ * 修饰在方法上将不能被重写
+ **/
+
+
+public class User1 {
+    int username;
+    int password;
+
+    public final void login(int username, int password) {
+        if (username == 123 && password == 123) {
+            System.out.println("登录成功");
+        } else {
+            System.out.println("登录失败");
+        }
+    }
+
+}
+
+class Emp extends User1 {
+    int empNo;
+/**
+ * 加上final 就不能被冲写了
+ */
+//    public void login(int username, int password) {
+//        System.out.println("登录成功");
+//    }
+}
+
+class FinalDemo {
+    public static void main(String[] args) {
+        Emp e = new Emp();
+        e.login(1, 1);
+    }
+}
