8ef542dcff57beb0b641305fb96af4e5162757f0
李真 <lizhen04842@hellobike.com>
2018-11-17 21:10:29 +0800

    字符串与时间相互转换

---
+++ src/main/java/com/java/basis/Data/DateDemo11.java
@@ -0,0 +1,25 @@
+package com.java.basis.Data;
+
+import java.util.Date;
+
+/**
+ * @program: basis
+ * @description:
+ * @author: Mr.LI
+ * @create: 2018-11-17 17:30
+ **/
+
+public class DateDemo11 {
+    public static void main(String[] args) {
+        //创建日期对象
+        Date date = new Date();
+        System.out.println(date);
+
+        //计算昨天到现在的时间戳
+        long ms = 24 * 60 * 60 * 1000;
+        long cms= System.currentTimeMillis();
+        //获得昨天当前时间
+        Date date1 =new Date(cms-ms);
+        System.out.println(date1);
+    }
+}
