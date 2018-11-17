84c9ae424e8c3a448d714f58140809eb75a1eef8
李真 <lizhen04842@hellobike.com>
2018-11-18 00:03:11 +0800

    打印1-100直接的数字

---
+++ src/main/java/com/java/basis/where/Test14.java
@@ -0,0 +1,21 @@
+package com.java.basis.where;
+
+/**
+ * @program: basis
+ * @description:
+ * @author: Mr.LI
+ * @create: 2018-11-17 23:52
+ * <p>
+ * 打印1-100直接的数字
+ **/
+
+public class Test14 {
+    public static void main(String[] args) {
+        //打印1-100之间的数字
+        int i = 1;
+        while (i <= 100) {
+            System.out.println("当前数字是" + i);
+            i++;
+        }
+    }
+}
