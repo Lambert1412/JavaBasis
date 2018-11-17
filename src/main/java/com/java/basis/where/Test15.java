c08f433216e71e848becb680dbec3eec9f8cd261
李真 <lizhen04842@hellobike.com>
2018-11-18 00:09:19 +0800

    1-100之间的偶数和

---
+++ src/main/java/com/java/basis/where/Test15.java
@@ -0,0 +1,23 @@
+package com.java.basis.where;
+
+/**
+ * @program: basis
+ * @description:
+ * @author: Mr.LI
+ * @create: 2018-11-18 00:03
+ * 1-100之间的偶数和
+ **/
+
+public class Test15 {
+    public static void main(String[] args) {
+        int i = 1;
+        int sum = 0;
+        while (i <= 100) {
+            if (i % 2 == 0) {
+                sum += i;
+            }
+            i++;
+        }
+        System.out.println("1-100之间的偶数和是" + sum);
+    }
+}
