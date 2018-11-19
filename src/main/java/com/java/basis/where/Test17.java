66f40eb11b5a76fe11e09d3ef2a06e96fccd2db1
李真 <lizhen04842@hellobike.com>
2018-11-19 13:43:34 +0800

    1-100的数字打印

---
+++ src/main/java/com/java/basis/where/Test17.java
@@ -0,0 +1,21 @@
+package com.java.basis.where;
+
+/**
+ * @program: basis
+ * @description:
+ * @author: Mr.LI
+ * @create: 2018-11-19 13:37
+ *
+ *
+ * 1-100的数字打印
+ **/
+
+public class Test17 {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 100) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
