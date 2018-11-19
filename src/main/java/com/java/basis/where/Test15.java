3e0696a2550cdbaca0db20796bf319b057980dce
李真 <lizhen04842@hellobike.com>
2018-11-19 13:57:54 +0800

    update

--- src/main/java/com/java/basis/where/Test15.java
+++ src/main/java/com/java/basis/where/Test15.java
@@ -14,7 +14,8 @@ public class Test15 {
         int sum = 0;
         while (i <= 100) {
             if (i % 2 == 0) {
-                sum += i;
+                sum =sum+i;
+//                sum += i;
             }
             i++;
         }
