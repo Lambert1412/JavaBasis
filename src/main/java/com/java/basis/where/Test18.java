3667cc06a4f141f91191255fce252486a5d6c28f
李真 <lizhen04842@hellobike.com>
2018-11-19 13:50:08 +0800

    打印1-100 偶数

---
+++ src/main/java/com/java/basis/where/Test18.java
@@ -0,0 +1,20 @@
+package com.java.basis.where;
+
+/**
+ * @program: basis
+ * @description:
+ * @author: Mr.LI
+ * @create: 2018-11-19 13:49
+ * 打印1-100偶数
+ **/
+
+public class Test18 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 100; i = i + 2) {
+            System.out.println(i + "");
+            if (i / 10 == 0) {
+                System.out.println();
+            }
+        }
+    }
+}
